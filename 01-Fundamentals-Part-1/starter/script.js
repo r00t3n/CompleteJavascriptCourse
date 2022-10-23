763c23694afcce01eca2bbb10309070f44650ce2
Florin Spoialã <florinspoiala@Florins-MacBook-Pro.local>
2022-10-23 22:38:44 +0300

    Add new lectures to the script

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,23 @@
-let js = "Amazing!!!";
+/*let js = "Amazing!!!";
 
 if (js === "Amazing!!!") {
     alert("Javascript is fun");
 }
 
 console.log(84 + 545 + 5464 + 21);
+
+
+let javaScriptIsFun = true;
+
+console.log(javaScriptIsFun);
+
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "javaScriptIsFun");
+
+javaScriptIsFun = "Fun!";
+*/
+
+const ageJonas = 2037 - 1993;
+console.log(ageJonas);
+
