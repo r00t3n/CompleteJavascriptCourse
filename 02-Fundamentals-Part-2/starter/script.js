e2634f7f80abb7c5c1b9b3acbc817fa517d854c9
r00t3n <spoiala.florin3@gmail.com>
2022-11-07 13:13:48 +0200

    Add avg function.

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+
+// function logger() {
+//     console.log("My name is Florin!");
+// }
+
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(3, 0);
+// console.log(appleJuice);
+
+// function calcAge1(birthyear) {
+//     return 2022 - birthyear;
+// }
+
+// const age1 = calcAge1(1993);
+// console.log(age1);
+// console.log(calcAge1(1992));
+
+// ////
+
+// const calcAge2 = function (birthyear) {
+//     return 2037 - birthyear;
+// }
+
+
+const calcAvg = (a, b, c) => (a + b + c) / 3;
+
+// console.log(calcAvg(1, 2, 3));
+
+const avgDolphins = calcAvg(44, 23, 71);
+const avgKoalas = calcAvg(65, 54, 49);
